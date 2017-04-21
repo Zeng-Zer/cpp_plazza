c60ef30edbab774bff9e420c6a7401e068586f21
David Zeng <david.zeng@epitech.eu>
2017-04-22 00:34:01 +0200

    hotfix nbthread

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
-  std::cout << "nb of thread: " << nbThread << std::endl;
+  std::cout << "nb of thread: " << *nbThread << std::endl;
 
   return 0;
 }

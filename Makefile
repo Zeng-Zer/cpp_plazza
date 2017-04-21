284f49da5bb22a052023567fdcbe4b6e414e10c7
David Zeng <david.zeng@epitech.eu>
2017-04-21 19:57:56 +0200

    initial comit

---
+++ Makefile
@@ -0,0 +1,54 @@
+##
+## Makefile :D
+##
+## Made by David Zeng
+## Login   <zeng_d@epitech.net>
+##
+## Started on  Mon Oct 12 13:31:18 2015 David Zeng
+## Last update Sat Jul  2 21:56:26 2016 David Zeng
+##
+
+NAME :=		a.out
+SRCDIR :=	src/
+INCLUDE :=	include/
+
+SRC :=		main.cpp
+
+CXX :=		g++
+CXXFLAGS :=	-W -Wall -Wextra -g -std=c++14
+SRC :=		$(addprefix $(SRCDIR), $(SRC))
+OBJ :=		$(SRC:.cpp=.o)
+RM :=		rm -f
+
+DEFAULT :=	"\033[00;0m"
+GREEN :=	"\033[0;32;1m"
+RED :=		"\033[0;31;1m"
+CYAN :=		"\033[0;36;1m"
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	$(CXX) -o $(NAME) $(OBJ) $(CXXFLAGS) && \
+		echo -e $(GREEN)"[BIN]"$(CYAN) $(NAME)$(DEFAULT) || \
+		echo -e $(RED)"[XX]"$(DEFAULT) $(NAME)
+	for file in $(shell find . | cut -c 3- | grep -P ".*\.(cpp|hpp|c|h)"); \
+		do fgrep -niH -e TODO -e FIXME $$file --color=auto; done; true
+
+clean:
+	echo -e $(CYAN)"Cleaning $(NAME) tmp files..." $(DEFAULT)
+	$(RM) $(OBJ)
+
+fclean:	clean
+	echo -e $(CYAN)"Cleaning $(NAME) executable..." $(DEFAULT)
+	$(RM) $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
+
+.SILENT: all $(NAME) clean fclean re
+
+%.o: %.cpp
+	@$(CXX) -c $< -o $@ $(CXXFLAGS) $(foreach dir, $(INCLUDE), -I$(dir)) && \
+		echo -e $(GREEN)"[OK]"$(DEFAULT) $< || \
+		echo -e $(RED)"[KO]"$(DEFAULT) $<
---
+++ include/Option.hpp
@@ -0,0 +1,76 @@
+#ifndef OPTION_HPP_
+# define OPTION_HPP_
+
+# include <iostream>
+# include <functional>
+# include <cassert>
+
+template<class T>
+class Option {
+public:
+  constexpr Option() : _hasValue(false) {
+  }
+
+  constexpr Option(T value) : _value(value), _hasValue(true) {
+  }
+
+  constexpr T const& getOrElse(T const& def) const {
+    return _hasValue ? _value : def;
+  }
+
+  template<class Throwable>
+  constexpr T const& getOrThrow(Throwable const& e) const {
+    return _hasValue ? _value : throw e;
+  }
+
+  constexpr operator bool() const {
+    return _hasValue;
+  }
+
+  constexpr bool operator!() const {
+    return !_hasValue;
+  }
+
+  T operator*() {
+    assert(_hasValue);
+    return _value;
+  }
+
+  T *operator->() {
+    assert(_hasValue);
+    return &_value;
+  }
+
+  T operator*() const {
+    assert(_hasValue);
+    return _value;
+  }
+
+  T const *operator->() const {
+    assert(_hasValue);
+    return &_value;
+  }
+
+  template<class F>
+  constexpr auto fmap(F f) {
+    using U = decltype(f(this->_value));
+
+    if (_hasValue) {
+      return Option<U>(f(_value));
+    }
+
+    return Option<U>();
+  }
+
+  void foreach(std::function<void (T)> f) const {
+    if (_hasValue) {
+      f(_value);
+    }
+  }
+
+private:
+  T _value;
+  bool _hasValue;
+};
+
+#endif /* !OPTION_HPP_ */

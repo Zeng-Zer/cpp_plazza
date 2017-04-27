f5f9fd2b8d89de22f43156136a3cdc3806197bc2
David Zeng <david.zeng@epitech.eu>
2017-04-27 03:13:39 +0200

    deleted using namespace: it is forbidden :(

--- src/Process.cpp
+++ src/Process.cpp
@@ -10,17 +10,16 @@ Process::~Process() {
 }
 
 void Process::run() {
-  using namespace std::chrono;
   for (int i = 0; i < _nbThread; ++i) {
     createThread();
   }
 
-  time_point<system_clock> start;
-  start = system_clock::now();
+  std::chrono::time_point<std::chrono::system_clock> start;
+  start = std::chrono::system_clock::now();
 
   while (1) {
-    time_point<system_clock> now;
-    now = system_clock::now();
+    std::chrono::time_point<std::chrono::system_clock> now;
+    now = std::chrono::system_clock::now();
 
     Option<Task> task = receiveTask();
 
@@ -35,7 +34,8 @@ void Process::run() {
     // timeout if it hasn't been working for 5 seconds
     if (!_tasks.empty()) { // TODO ADD CONDITION VAR ?
       start = now;
-    } else if (duration_cast<milliseconds>(now - start).count() >= 5000) {
+    } else if (std::chrono::duration_cast<std::chrono::milliseconds>
+	       (now - start).count() >= 5000) {
       timeoutSuicide();
     }
 

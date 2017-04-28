b67394edf731e2d9e4a4d596c12f4b380393dbb2
Jamy Bailly <jamy.bailly@epitech.eu>
2017-04-29 00:50:05 +0200

    add main test

---
+++ testCom.cpp
@@ -0,0 +1,38 @@
+#include "Communication.hpp"
+#include <iostream>
+
+int main()
+{
+  Communication yolo(0);
+
+  Package test;
+
+  test.type = QUIT;
+  test.content.value = 10000;
+
+  int pid = fork();
+  if (pid == 0)
+    {
+      usleep(10000);
+      std::cout << "child" << std::endl;
+      yolo.openCommunicationChild();
+      std::cout << "receiving" << std::endl;
+      Package testo = yolo.receiveMsg();
+      std::cout << "received!" << std::endl;
+      if (!(testo.type == UNDEFINED || test.content.value == -1))
+	std::cout << testo.content.value << std::endl;
+      // else
+      // 	std::cout << "yolo-fzepojfzep" << std::endl;
+    }
+  else
+    {
+      std::cout << "parent" << std::endl;
+      yolo.openCommunicationMain();
+      usleep(999999);
+      std::cout << "sending" << std::endl;
+      yolo.sendMsg(test);
+    }
+  yolo.close();
+  yolo.rmfifo();
+  return (0);
+}
